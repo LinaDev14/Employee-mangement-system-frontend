ec4976c1494f299495301043a129776583fcbbce
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-08 20:29:43 -0500

    se crea component AddEmployee

---
+++ src/components/AddEmployee.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AddEmployee = () => {
+  return (
+      <div className="container_employee">
+            <div className="employee">
+                    <div className="title_employee">
+                          <h1>Add New Employee</h1>
+                  </div>
+
+                    <div className="">
+                    </div>
+            </div>
+      </div>
+)
+}
+
+export {AddEmployee}
