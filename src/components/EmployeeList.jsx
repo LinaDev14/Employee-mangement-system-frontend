43f1fc5d8921dee7155b0c5e680da076e0c24351
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-09 17:04:17 -0500

    se agregan estilos a la tabla con tailwind CSS

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -4,10 +4,26 @@ const EmployeeList = () => {
       return (
             <div className="container_list">
                   <div className="container_button">
-                        <button className="add_employee">Add Employee</button>
+                        <button className="add_employee uppercase tracking-wider py-3 px-4">Add Employee</button>
                   </div>
 
                   <div className='border_bottom'>
+
+                        <table className="min-w-full">
+
+                              <thead className="bg-gray-100">
+                                    <tr>
+                                          <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-4">First Name</th>
+                                          <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-4">Last Name</th>
+                                          <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-4">Email</th>
+                                          <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-4">Phone Number</th>
+                                          <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-4">Country</th>
+                                          <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-4">Actions</th>
+                                    </tr>
+                        
+                              </thead>
+                        
+                        </table>
                   </div>
             </div>
       )
