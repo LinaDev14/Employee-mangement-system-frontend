64b3f4bdd621d00cb71388dac39d212345acd538
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-09 14:40:17 -0500

    se crea service para POST

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import EmployeeService from '../services/EmployeeService';
 
 const AddEmployee = () => {
 
@@ -26,7 +27,12 @@ const AddEmployee = () => {
 
     const saveEmployee = (e) => {
         e.preventDefault();
-        
+        EmployeeService.saveEmployee(employee)
+        .then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            console.log(error)
+        })
     }
 
 
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,16 @@
+import axios from "axios"
+
 const EMPLOYEE_API_BASE_URL = "localhost:8080/api/v1/employees";
 
-class EmployeeService { }
+class EmployeeService { 
+
+      saveEmployee(employee) {
+            
+            return axios.post(EMPLOYEE_API_BASE_URL, employee);
+
+      }
+
+}
 
 
 
