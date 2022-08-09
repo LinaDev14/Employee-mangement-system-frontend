7a3f34459424d9e696ec96895f481b82c48df1be
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-09 13:15:06 -0500

    se agregan funciones handleChange y SaveEmployee

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddEmployee = () => {
+
+    const [employee, setEmployee] = useState({
+    
+            id: "",
+            firstName:"",
+            lastName: "",
+            emailId: "",
+            numberPhone: "",
+            country: ""
+    });
+
+
+    const handleChange = (e) =>{
+
+        const value = e.target.value;
+
+        setEmployee({
+            ...employee, [
+                e.target.name
+            ]: value
+        });
+    }
+
+    const saveEmployee = (e) => {
+        e.preventDefault();
+    }
+
+
   return (
       <div className="container_employee">
             <div className="employee">
@@ -9,27 +37,57 @@ const AddEmployee = () => {
                   </div>
                     <div className="label_name">
                         <label>First Name: </label>
-                        <input type="text" className="input_field"/>
+                  <input
+                      type="text" className="input_field"
+                      name="firstName"
+                      value={employee.firstName}
+                      onChange={(e) => handleChange(e)}
+                    
+                      
+                  />
                     </div>
                     <div className="label_name">
                         <label>Last Name: </label>
-                        <input type="text" className="input_field"/>
+                  <input
+                      type="text" className="input_field"
+                      name="lastName"
+                      value={employee.lastName}
+                      onChange={(e) => handleChange(e)}
+                  
+                  />
                     </div>
                     <div className="label_name">
                         <label>Email: </label>
-                        <input type="email" className="input_field"/>
+                  <input
+                      type="email" className="input_field" 
+                      name="emailId"
+                      value={employee.emailId}
+                      onChange={(e) => handleChange(e)}
+                      
+                  />
                     </div>
                     <div className="label_name">
                         <label>Number Phone: </label>
-                        <input type="number" className="input_field"/>
-                    </div>
+                  <input type="number" className="input_field" />
+                  name="phoneNumber"
+                  value={employee.numberPhone}
+                  onChange={(e) => handleChange(e)}
+              </div>
+              
                     <div className="label_name">
                         <label>Country: </label>
-                        <input type="text" className="input_field"/>
+                        <input 
+                        type="text" className="input_field"
+                      name="country"
+                      value={employee.country}
+                      onChange={(e) => handleChange(e)}
+                  />
                     </div>
 
                     <div className="label_name">
-                        <button className="save_employee">Save</button>
+                        <button className="save_employee"
+                        onClick={saveEmployee}
+                        >Save</button>
                         <button className="save_employee_clear">Clear</button>
                     </div>
 
