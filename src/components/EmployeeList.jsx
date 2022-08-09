ec9d74096523471a951814388bd91f03f29985a9
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-09 18:14:39 -0500

    se agrega useEffect

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,10 +1,35 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
+import EmployeeService from '../services/EmployeeService';
 
 const EmployeeList = () => {
 
       const navigate = useNavigate();
+
+      const [loading, setLoading] = useState(true);
+      const [employees, setEmployees] = useState(null);
+
+      useEffect(() => {
+            const fetchData = async() => {
+
+                  setLoading(true);
+
+                  try {
+                        const response = await EmployeeService.getEmployees();
+
+                        setEmployees(response.data)
+
+                  }catch(error){
+                        console.log(error)
+                  }
+
+                  setLoading(false);
+            };
+
+            fetchData();
+      }, [])
+      
 
       return (
             <div className="container_list">
@@ -30,38 +55,41 @@ const EmployeeList = () => {
                                           <th className="text-right font-medium text-gray-500 uppercase tracking-wider py-3 px-4">Actions</th>
                                     </tr>
                               </thead>
+                              {!loading && (
+                                    <tbody className="bg-white">
+                                          {employees.map((employee) => {
+                                                <tr key={employee.id}>
+                                                      <td className="text-left px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm text-gray-500">employee.firstName</div>
+                                                      </td>
 
-                              <tbody>
-                                    <tr>
-                                          <td className="text-left px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-500">Lina Maria</div>
-                                          </td>
+                                                      <td className="text-left px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm text-gray-500">employee.lastName</div>
+                                                      </td>
 
-                                          <td className="text-left px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-500">Guerrero Lopez</div>
-                                          </td>
+                                                      <td className="text-left px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm text-gray-500">employee.email</div>
+                                                      </td>
 
-                                          <td className="text-left px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-500">holamundo@gmail.com</div>
-                                          </td>
+                                                      <td className="text-left px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm text-gray-500">employee.phoneNumber</div>
+                                                      </td>
 
-                                          <td className="text-left px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-500">3125648569</div>
-                                          </td>
+                                                      <td className="text-left px-6 py-4 whitespace-nowrap">
+                                                            <div className="text-sm text-gray-500">employee.country</div>
+                                                      </td>
 
-                                          <td className="text-left px-6 py-4 whitespace-nowrap">
-                                                <div className="text-sm text-gray-500">Colombia</div>
-                                          </td>
-
-                                          <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
-                                                <a href="#" className="text-indigo-600 hover:text-indigo-800 px-4">Edit</a>
-                                                <a href="#" className="text-red-600 hover:text-red-800">Delete</a>
+                                                      <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
+                                                            <a href="#" className="text-indigo-600 hover:text-indigo-800 px-4">Edit</a>
+                                                            <a href="#" className="text-red-600 hover:text-red-800">Delete</a>
                                                 
-                                          </td>
+                                                      </td>
 
-                                    </tr>
+                                                </tr>
+                                          })}
                               </tbody>
-                        
+                                          
+                              )}
                         </table>
                   </div>
             </div>
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -4,10 +4,14 @@ const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
 
 class EmployeeService { 
 
+      // post
       saveEmployee(employee) {
-            
             return axios.post(EMPLOYEE_API_BASE_URL, employee);
+      }
 
+      // get
+      getEmployees() {
+            return axios.get(EMPLOYEE_API_BASE_URL)
       }
 
 }
