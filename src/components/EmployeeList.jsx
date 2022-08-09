797ba35de6ddb8c4a1520aee54b247007c5aabfd
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-09 16:46:43 -0500

    fix bugs en employeeList

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const EmployeeList = () => {
-  return (
-    <div>EmployeeList</div>
-  )
+      return (
+            <div className="container_list">
+                  <div className="container_button">
+                        <button className="add_employee">Add Employee</button>
+                  </div>
+
+                  <div className='border_bottom'>
+                  </div>
+            </div>
+      )
 }
 
 export {EmployeeList}
